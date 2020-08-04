8d170a21b9be23714d33392578f1e7930e886d21
Benny <johansson.benny@gmail.com>
2020-08-04 09:27:00 +0200

    set permiss

--- setPermissions.sh
+++ setPermissions.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+touch /var/app/current/myDB/Bennyworld.db
+touch /var/app/current/myDB/Bennyland.db
+touch /var/app/current/myDB/Saraland.db
 sudo chown webapp /var/app/current/myDB/*
 sudo chmod a+w /var/app/current/myDB/*.db
