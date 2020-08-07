6c710c48b3265ddbc8682b58500821b127c0aac2
Benny <johansson.benny@gmail.com>
2020-08-07 15:20:05 +0200

    salary to cash

--- setPermissions.sh
+++ setPermissions.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+sudo chown ec2-user /var/app/current/myDB/
 touch /var/app/current/myDB/Bennyworld.db
 touch /var/app/current/myDB/Bennyland.db
 touch /var/app/current/myDB/Saraland.db
 sudo chown webapp /var/app/current/myDB/*
 sudo chmod a+w /var/app/current/myDB/*.db
+
