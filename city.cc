e037bc53941cc14c71642f5f9740fca18a125ce2
Benny <johansson.benny@gmail.com>
2020-08-15 08:58:22 +0200

    parameter

--- city.cc
+++ city.cc
@@ -1295,7 +1295,7 @@ void City::adjust_money()
     double inflation = 0;
     double item_inflation = 0;
     double scale_factor = 0.5;
-    double target_inflation = 0.02;
+    double target_inflation = 0.01;
 
     double MAX_CHANGE_FACTOR = 0.6;
 
